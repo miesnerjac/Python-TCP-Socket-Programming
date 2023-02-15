7bce1709222d8ba1b0cdfd9d252e37762a1773f3
jmmgk9 <jmmgk9@umsystem.edu>
2023-02-14 21:50:38 -0500

    client working as intended

--- client_browser.py
+++ client_browser.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
+    Jacob Miesner
+    Missouri University of Science and Technology
+    CS 3610
+    Project 01
+    9 Feb 2023
 Run with the following command line parameters:
 python3 client_browser.py hostname port file
 
@@ -20,17 +25,23 @@ if len(sys.argv) != 4:
 else:
     # Dele the pass and do your arg parsing here
     # Hint, you may need to get an IP from a hostame.
-    pass
+    server_hostname = sys.argv[1]
+    server_port = int(sys.argv[2])
+    file_name = sys.argv[3]
+    server_ip = socket.gethostbyname(server_hostname)
 
 try:
     client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     # Delete the pass and make your GET request here
-    pass
+    client_socket.connect((server_hostname, server_port))
+    request = "GET /" + file_name + " HTTP/1.1\r\n" + "Host: " + server_hostname + "\r\n\r\n"
+    client_socket.send(request.encode())
 
     # Delete the pass and parse the return data here
     # Hint: a loop helps to make sure you got all the data
     # Just print what's returned from the server
-    pass
+    data = client_socket.recv(1024).decode()
+    print(data)
 
 except Exception as e:
     print("Exception was: ", e)
